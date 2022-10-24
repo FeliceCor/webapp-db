b2de591415f13b8074ed3ee4672020ae1c7edb62
Felice <felice.coraci@gmail.com>
2022-10-24 16:55:41 +0200

    aggiunta tabella telefono
    aggiunta tabella telefono

--- db.sql
+++ db.sql
@@ -1,6 +1,13 @@
 -- database
 
 CREATE TABLE persona(
-	id INT PRIMARY KEY,
+	id INT AUTO_INCREMENT PRIMARY KEY,
 	nome VARCHAR(50) NOT NULL
 );
+
+CREATE TABLE telefoni(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+	telefono VARCHAR(50) NOT NULL
+);
+
+-- commento
